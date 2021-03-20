15826878cc220e9580e468377a6e4c01abd69caf
Franciel Leandro <francielleandro@gmail.com>
2021-03-20 04:52:00 -0300

    Projeto inicial

---
+++ ByteBank.Sistema/Frm_Principal.vb
@@ -0,0 +1,17 @@
+﻿Public Class Frm_Principal
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.Text = "Fomulário Principal"
+        Lbl_Principal.Text = "Fomulário Principal"
+        Btn_Principal.Text = "Clique Aqui"
+
+    End Sub
+
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+
+    End Sub
+End Class
