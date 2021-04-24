9a4b20c212afafba12d932c993749f8a924fa06e
Franciel Leandro <francielleandro@gmail.com>
2021-04-24 02:44:40 -0300

    trabalhando com objetos

---
+++ ByteBank.Sistema/Frm_Principal3b.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal3b
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn_Principal = New System.Windows.Forms.Button()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.Btn_Principal03a = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn_Principal
+        '
+        Me.Btn_Principal.Location = New System.Drawing.Point(15, 56)
+        Me.Btn_Principal.Name = "Btn_Principal"
+        Me.Btn_Principal.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Principal.TabIndex = 8
+        Me.Btn_Principal.Text = "Button1"
+        Me.Btn_Principal.UseVisualStyleBackColor = True
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(12, 14)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Principal.TabIndex = 7
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'Btn_Principal03a
+        '
+        Me.Btn_Principal03a.Location = New System.Drawing.Point(15, 86)
+        Me.Btn_Principal03a.Name = "Btn_Principal03a"
+        Me.Btn_Principal03a.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Principal03a.TabIndex = 9
+        Me.Btn_Principal03a.Text = "Button1"
+        Me.Btn_Principal03a.UseVisualStyleBackColor = True
+        '
+        'Frm_Principal3b
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(350, 235)
+        Me.Controls.Add(Me.Btn_Principal03a)
+        Me.Controls.Add(Me.Btn_Principal)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Name = "Frm_Principal3b"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Frm_Principal3b"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Btn_Principal As Button
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents Btn_Principal03a As Button
+End Class
---
+++ ByteBank.Sistema/Frm_Principal3b.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Text.RegularExpressions
+Imports ByteBank.Bibliotecas.Classes.Clientes
+Imports ByteBank.Bibliotecas.Classes.Funcionarios
+
+Public Class Frm_Principal3b
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.Text = "Formulario Principal 3b"
+        Lbl_Principal.Text = "Formulario Principal 3b"
+        Btn_Principal.Text = "Testar"
+        Btn_Principal03a.Text = "Clique Aqui"
+    End Sub
+
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+        Dim vFrase As String = "O telefone é 23344332"
+        'Dim vExpressaoRegular As String = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+        'Dim vExpressaoRegular As String = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]"
+        'Dim vExpressaoRegular As String = "[0-9]{5}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-]?[0-9]{4}"
+        Dim vExpressaoRegular As String = "[0-9]{4,5}-?[0-9]{4}"
+
+        Dim X As Boolean = Regex.IsMatch(vFrase, vExpressaoRegular)
+        MsgBox(X)
+
+        Dim Y As Match = Regex.Match(vFrase, vExpressaoRegular)
+        MsgBox(Y.Value)
+    End Sub
+
+    Private Sub Btn_Principal03a_Click(sender As Object, e As EventArgs) Handles Btn_Principal03a.Click
+
+        Dim Cliente_1 As New Cliente()
+        Dim Cliente_2 As New Cliente()
+
+        Cliente_1.nome = "Carlos"
+        Cliente_1.cpf = "123.123.123-23"
+        Cliente_1.profissao = "Analista de Sistemas"
+
+        Cliente_2.nome = "Carlos"
+        Cliente_2.cpf = "123.123.123-23"
+        Cliente_2.profissao = "Analista de Sistemas"
+
+        Dim Ger As New Gerente("123.123.123-23")
+        Dim KK As String = "aaaaaaaaaaaaaaaaaaaa"
+
+        If (Cliente_1.Equals(Cliente_2)) Then
+            MsgBox("SÃO IGUAIS !!!!")
+        Else
+            MsgBox("NÃO SÃO IGUAIS !!!!")
+        End If
+    End Sub
+End Class
