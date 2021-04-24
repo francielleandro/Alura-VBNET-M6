c5d4008db9153fdf2d4a50a9f6046f0abdbec573
Franciel Leandro <francielleandro@gmail.com>
2021-04-24 01:30:49 -0300

    Adicionando novo formulario

---
+++ ByteBank.Sistema/Frm_Principal3.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.Btn_Principal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(12, 9)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Principal.TabIndex = 0
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'Btn_Principal
+        '
+        Me.Btn_Principal.Location = New System.Drawing.Point(15, 54)
+        Me.Btn_Principal.Name = "Btn_Principal"
+        Me.Btn_Principal.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Principal.TabIndex = 1
+        Me.Btn_Principal.Text = "Button1"
+        Me.Btn_Principal.UseVisualStyleBackColor = True
+        '
+        'Frm_Principal3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 257)
+        Me.Controls.Add(Me.Btn_Principal)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Name = "Frm_Principal3"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Frm_Principal3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents Btn_Principal As Button
+End Class
---
+++ ByteBank.Sistema/Frm_Principal3.vb
@@ -0,0 +1,14 @@
+﻿Public Class Frm_Principal3
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Me.Text = "Formulario Principal 3"
+        Lbl_Principal.Text = "Formulario Principal 3"
+        Btn_Principal.Text = "Clique Aqui"
+
+    End Sub
+End Class
