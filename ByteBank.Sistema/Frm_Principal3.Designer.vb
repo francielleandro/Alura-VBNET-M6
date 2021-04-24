04cb4ede7b8a0bd43ec643635b5f68440d76cebe
Franciel Leandro <francielleandro@gmail.com>
2021-04-24 02:01:54 -0300

    utilizando expressoes regulares

--- ByteBank.Sistema/Frm_Principal3.Designer.vb
+++ ByteBank.Sistema/Frm_Principal3.Designer.vb
@@ -24,6 +24,11 @@ Partial Class Frm_Principal3
     Private Sub InitializeComponent()
         Me.Lbl_Principal = New System.Windows.Forms.Label()
         Me.Btn_Principal = New System.Windows.Forms.Button()
+        Me.Txt_Verificar = New System.Windows.Forms.TextBox()
+        Me.Lbl_Texto = New System.Windows.Forms.Label()
+        Me.Lbl_Expressao = New System.Windows.Forms.Label()
+        Me.Txt_Expressao = New System.Windows.Forms.TextBox()
+        Me.Lbl_Padrao = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Lbl_Principal
@@ -37,18 +42,64 @@ Partial Class Frm_Principal3
         '
         'Btn_Principal
         '
-        Me.Btn_Principal.Location = New System.Drawing.Point(15, 54)
+        Me.Btn_Principal.Location = New System.Drawing.Point(362, 222)
         Me.Btn_Principal.Name = "Btn_Principal"
         Me.Btn_Principal.Size = New System.Drawing.Size(75, 23)
         Me.Btn_Principal.TabIndex = 1
         Me.Btn_Principal.Text = "Button1"
         Me.Btn_Principal.UseVisualStyleBackColor = True
         '
+        'Txt_Verificar
+        '
+        Me.Txt_Verificar.Location = New System.Drawing.Point(12, 58)
+        Me.Txt_Verificar.Name = "Txt_Verificar"
+        Me.Txt_Verificar.Size = New System.Drawing.Size(425, 20)
+        Me.Txt_Verificar.TabIndex = 2
+        '
+        'Lbl_Texto
+        '
+        Me.Lbl_Texto.AutoSize = True
+        Me.Lbl_Texto.Location = New System.Drawing.Point(12, 42)
+        Me.Lbl_Texto.Name = "Lbl_Texto"
+        Me.Lbl_Texto.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Texto.TabIndex = 3
+        Me.Lbl_Texto.Text = "Label1"
+        '
+        'Lbl_Expressao
+        '
+        Me.Lbl_Expressao.AutoSize = True
+        Me.Lbl_Expressao.Location = New System.Drawing.Point(12, 110)
+        Me.Lbl_Expressao.Name = "Lbl_Expressao"
+        Me.Lbl_Expressao.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Expressao.TabIndex = 5
+        Me.Lbl_Expressao.Text = "Label1"
+        '
+        'Txt_Expressao
+        '
+        Me.Txt_Expressao.Location = New System.Drawing.Point(12, 126)
+        Me.Txt_Expressao.Name = "Txt_Expressao"
+        Me.Txt_Expressao.Size = New System.Drawing.Size(425, 20)
+        Me.Txt_Expressao.TabIndex = 4
+        '
+        'Lbl_Padrao
+        '
+        Me.Lbl_Padrao.AutoSize = True
+        Me.Lbl_Padrao.Location = New System.Drawing.Point(15, 153)
+        Me.Lbl_Padrao.Name = "Lbl_Padrao"
+        Me.Lbl_Padrao.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Padrao.TabIndex = 6
+        Me.Lbl_Padrao.Text = "Label1"
+        '
         'Frm_Principal3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(449, 257)
+        Me.Controls.Add(Me.Lbl_Padrao)
+        Me.Controls.Add(Me.Lbl_Expressao)
+        Me.Controls.Add(Me.Txt_Expressao)
+        Me.Controls.Add(Me.Lbl_Texto)
+        Me.Controls.Add(Me.Txt_Verificar)
         Me.Controls.Add(Me.Btn_Principal)
         Me.Controls.Add(Me.Lbl_Principal)
         Me.Name = "Frm_Principal3"
@@ -61,4 +112,9 @@ Partial Class Frm_Principal3
 
     Friend WithEvents Lbl_Principal As Label
     Friend WithEvents Btn_Principal As Button
+    Friend WithEvents Txt_Verificar As TextBox
+    Friend WithEvents Lbl_Texto As Label
+    Friend WithEvents Lbl_Expressao As Label
+    Friend WithEvents Txt_Expressao As TextBox
+    Friend WithEvents Lbl_Padrao As Label
 End Class
--- ByteBank.Sistema/Frm_Principal3.vb
+++ ByteBank.Sistema/Frm_Principal3.vb
@@ -1,4 +1,8 @@
-﻿Public Class Frm_Principal3
+﻿Imports System.Text.RegularExpressions
+
+Public Class Frm_Principal3
+
+    Public Property expression As String = "[0-9]{4,5}-?[0-9]{4}"
     Public Sub New()
 
         ' This call is required by the designer.
@@ -8,7 +12,34 @@
 
         Me.Text = "Formulario Principal 3"
         Lbl_Principal.Text = "Formulario Principal 3"
-        Btn_Principal.Text = "Clique Aqui"
+        Btn_Principal.Text = "Testar expressão regular!"
+        Lbl_Texto.Text = "Texto a ser verificado"
+        Lbl_Expressao.Text = "Expressão regular"
+        Lbl_Padrao.Text = "O padrão é: " + expression
+    End Sub
 
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+        Dim vFrase As String = Txt_Verificar.Text
+        'Dim vExpressaoRegular As String = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+        'Dim vExpressaoRegular As String = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]"
+        'Dim vExpressaoRegular As String = "[0-9]{5}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-]?[0-9]{4}"
+
+        Dim exp = Txt_Expressao.Text
+
+        Dim vExpressaoRegular As String
+
+        If String.IsNullOrEmpty(exp) Then
+            vExpressaoRegular = expression
+        Else
+            vExpressaoRegular = exp
+        End If
+
+        Dim X As Boolean = Regex.IsMatch(vFrase, vExpressaoRegular)
+        MsgBox(X)
+
+        Dim Y As Match = Regex.Match(vFrase, vExpressaoRegular)
+        MsgBox(Y.Value)
     End Sub
 End Class
